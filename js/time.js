6230ae6e877ebe60110fecf87b67077afec50a55
Squirrel <squirrelhomebrew@gmail.com>
2025-11-15 01:26:58 -0800

    add christmas stuff

--- js/time.js
+++ js/time.js
@@ -13,5 +13,32 @@ function upTheTitle() {
     }
 }
 
+function christmas() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const christmas = new Date(year, 11, 25);
+    const titleNumber = document.getElementById('christmas-title-number');
+
+    if (now.getMonth() === 11 && now.getDate() === 25) {
+        document.getElementById("christmas-number").textContent = "Merry Christmas!!!";
+        document.querySelector("#christmas-countdown span:nth-child(2)").style.display = "none";
+        if (titleNumber) titleNumber.textContent = "Merry Christmas!!!";
+        return;
+    }
+
+    let nextChristmas = christmas;
+    if (now > christmas) {
+        nextChristmas = new Date(year + 1, 11, 25);
+    }
+
+    const diff = nextChristmas - now;
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+    document.getElementById("christmas-number").textContent = days;
+    document.querySelector("#christmas-countdown span:nth-child(2)").style.display = "block";
+    if (titleNumber) titleNumber.textContent = days;
+}
+
+christmas();
 setInterval(upTheTitle, 1000);
 upTheTitle();
