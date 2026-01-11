b83613bbbe08693452235dda5fe04542bb3c1847
Squirrel <squirrelhomebrew@gmail.com>
2026-01-10 18:26:20 -0800

    website go bye bye

--- js/christmas-image.js
+++
@@ -1,9 +0,0 @@
-const allBgs = [
-    'img/christmas/christmas.jpg',
-    'img/christmas/christmas.webp',
-    'img/christmas/christmas2.webp',
-    'img/christmas/christmas3.png',
-    'img/christmas/christmas4.jpg',
-];
-const randin = Math.floor(Math.random() * allBgs.length);
-document.getElementById('christmas-bg').src = allBgs[randin];
