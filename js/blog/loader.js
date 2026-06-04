1a591c854bf86ec2a3d5d18351ae30abdbbcbfdd
squirrel <squirrelhomebrew@gmail.com>
2026-06-03 18:37:20 -0700

    add like button to blogs

--- js/blog/loader.js
+++ js/blog/loader.js
@@ -1,4 +1,8 @@
-const slug = window.location.pathname.replace(/^\/blog\//, '').replace(/\/$/, '');
+const slug = window.location.pathname
+    .replace(/^\/blog\//, '')
+    .replace(/\/$/, '')
+    .replace(/[^\w-]/g, '_');
+
 const content = document.getElementById('post-content');
 
 function formatDate(dateStr) {
@@ -6,43 +10,78 @@ function formatDate(dateStr) {
     return `${m}/${d}/${y}`;
 }
 
+function createLikeButton() {
+    return `
+        <div class="blog-likebtn blog-likebtn-bottom">
+            <span
+                class="likebtn-wrapper"
+                data-theme="greenred"
+                data-identifier="squirrelz_blog_${slug}">
+            </span>
+        </div>
+    `;
+}
+
+function loadLikeBtn() {
+    if (document.getElementById('likebtn_wjs')) {
+        if (window.LikeBtn) window.LikeBtn.init();
+        return;
+    }
+
+    const script = document.createElement('script');
+    script.id = 'likebtn_wjs';
+    script.async = true;
+    script.src = '//w.likebtn.com/js/w/widget.js';
+    document.head.appendChild(script);
+}
+
 if (!slug) {
     window.location.href = '/blog';
 } else {
     Promise.all([
         fetch('/blog/posts.json').then(r => r.json()),
-        fetch(`/blog/posts/${slug}.md`).then(r => {
+        fetch(`/blog/posts/${slug.replace(/_/g, '/')}.md`).then(r => {
             if (!r.ok) throw new Error('not found');
             return r.text();
         })
     ])
-        .then(([posts, md]) => {
-            const post = posts.find(p => p.slug === slug);
-            if (post) document.title = `${post.title} - blog`;
+    .then(([posts, md]) => {
+        const post = posts.find(p => p.slug === slug.replace(/_/g, '/'));
 
-            const title = post?.title ?? '';
-            const date = post ? formatDate(post.date) : '';
+        if (post) {
+            document.title = `${post.title} - blog`;
+        }
 
-            content.innerHTML = `
-                    <div class="blog-post-header">
-                        <div class="blog-post-header-top">
-                            <a class="blog-post-back" href="/blog">back</a>
-                            <span class="blog-post-bar-sep">|</span>
-                            <div class="blog-post-title-wrap">
-                                <span class="blog-post-title-marquee" id="post-title-bar">${title}</span>
-                            </div>
-                            <span class="blog-post-bar-sep">|</span>
-                            <span class="blog-post-bar-date">${date}</span>
-                        </div>
-                        <hr class="blog-post-header-hr">
+        const title = post?.title ?? '';
+        const date = post ? formatDate(post.date) : '';
+
+        content.innerHTML = `
+            <div class="blog-post-header">
+                <div class="blog-post-header-top">
+                    <a class="blog-post-back" href="/blog">back</a>
+                    <span class="blog-post-bar-sep">|</span>
+                    <div class="blog-post-title-wrap">
+                        <span class="blog-post-title-marquee" id="post-title-bar">${title}</span>
                     </div>
-                    <div class="blog-post-body">${marked.parse(md)}</div>
-                `;
+                    <span class="blog-post-bar-sep">|</span>
+                    <span class="blog-post-bar-date">${date}</span>
+                </div>
+                <hr class="blog-post-header-hr">
+            </div>
 
-            const titleEl = document.getElementById('post-title-bar');
-            if (titleEl) maybeMarquee(titleEl, title);
-        })
-        .catch(() => {
-            content.innerHTML = '<p style="color:#888;font-size:14px;">could not load post.,</p>';
-        });
+            <div class="blog-post-body">
+                ${marked.parse(md)}
+            </div>
+
+            ${createLikeButton()}
+        `;
+
+        const titleEl = document.getElementById('post-title-bar');
+        if (titleEl) maybeMarquee(titleEl, title);
+
+        loadLikeBtn();
+    })
+    .catch(() => {
+        content.innerHTML = '<p style="color:#888;font-size:14px;">could not load post.,</p>';
+    });
 }
